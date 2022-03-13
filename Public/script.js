a054881697c16af427c91ca5eedc331bdc6949a0
Sanya <sanya@code.com>
2022-03-13 10:13:48 +0530

    commit

--- Public/script.js
+++ Public/script.js
@@ -67,6 +67,13 @@ $(document).ready(function(){
         $('.circlechart').circlechart();
        
       })
-  
+      $( ".mr-auto .nav-item" ).bind( "click", function(event) {
+        event.preventDefault();
+        var clickedItem = $( this );
+        $( ".mr-auto .nav-item" ).each( function() {
+            $( this ).removeClass( "active" );
+        });
+        clickedItem.addClass( "active" );
+    });
 });
 
